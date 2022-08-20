333dc11c46884cf2b5564c8b7e8dd4e944484142
Ivaylo Gashev <ivaylo.gashev@knab.nl>
2022-08-20 22:49:18 +0300

    Initial Commit

---
+++ Teslawsome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Teslawsome
+//
+//  Created by Ivaylo Gashev on 20.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Teslawsome/TeslawsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslawsomeApp.swift
+//  Teslawsome
+//
+//  Created by Ivaylo Gashev on 20.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TeslawsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
